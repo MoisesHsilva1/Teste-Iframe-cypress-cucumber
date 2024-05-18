a676e79cc34d1cfeff8af2fda564e33100b5fe6f
Moises Henrique <moihgm67@gmail.com>
2024-05-18 00:55:02 -0300

    Atualizando projeto

--- cypress/e2e/Cucumber/Tests/Iframe.cy.js
+++ cypress/e2e/Cucumber/Tests/Iframe.cy.js
@@ -1,2 +1,17 @@
-import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps"
+import {Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 import VerificarIframe from "../Pages/VerifcandoIframe.cy";
+
+const verifcandoIframe = new VerificarIframe();
+
+Given(' estou na página do iframe', () => {
+    cy.visitandoSite()
+})  
+
+When('eu acessar o iframe', () => {
+
+})
+
+Then('o conteúdo do iframe deve conter "Your content goes here."', () => {
+
+})
+
